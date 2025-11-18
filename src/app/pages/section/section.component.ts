97d55f21006c3f14ad78e8d896794806cd2d7cba
fatemZahrae <kribiif@gmail.com>
2025-11-18 14:15:55 +0100

    add frontend template

---
+++ src/app/pages/section/section.component.ts
@@ -0,0 +1,15 @@
+import { Component,OnInit } from '@angular/core';
+
+declare var google: any;
+
+@Component({
+    moduleId: module.id,
+    selector: 'maps-cmp',
+    templateUrl: 'section.component.html'
+})
+
+export class SectionComponent implements OnInit {
+    ngOnInit() {
+
+    }
+}
---
+++ src/polyfills.ts
@@ -0,0 +1,5 @@
+
+import '@angular/localize/init';
+
+import "zone.js/dist/zone"; // Included with Angular CLI.
+
